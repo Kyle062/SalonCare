3a81a13b910f269e496990286558dd26a359b2ee
Kyle Bernard Trimidal <kylealba0624@gmail.com>
2025-11-21 14:14:59 +0800

    first Commit

---
+++ gui/Login.java
@@ -0,0 +1,14 @@
+package gui;
+
+import javax.swing.JFrame;
+
+public class Login {
+
+    public static void main(String[] args) {
+        JFrame mainFrame = new JFrame();
+        mainFrame.setVisible(true);
+        mainFrame.setSize(1000, 800);
+        mainFrame.setLocationRelativeTo(null);
+
+    }
+}
